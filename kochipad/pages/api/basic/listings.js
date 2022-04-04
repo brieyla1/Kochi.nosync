d655e686ff9ca3c0ce7d9144c264e91b102612dc
Lancelot de Briey <Business@lancedb.dev>
2022-04-04 04:19:58 +0200

    removed backend from nextjs and added a few packages

--- kochipad/pages/api/basic/listings.js
+++
@@ -1,8 +0,0 @@
-const state = require('/pages/api/state.js');
-
-// next js api function
-const getListingList = async () => {
-  return await state.functions.getListingList();
-};
-
-module.exports = { getListingList };
